bc579af95d94e3d727cba8960487423d0c5ff2fa
Houley <ethouley@gmail.com>
2019-03-14 13:25:10 +1000

    Completed Exercise 2
    Topic 2 Exercise are done

--- ams/topic2/Exercise2.cs
+++ ams/topic2/Exercise2.cs
@@ -4,19 +4,31 @@ class MilesPerHourTable
     public static void Main()
     {
         bool quit = false;
-        string temp_user_input;
-        int conversion_rates = 0.62137, user_input; // 1 km = 0.62137
+        string temp_number_of_rows;
+        double conversion_rates = 0.62137;
+        double kms;
+        int number_of_rows = 0, miles = 15;
+        char quit_char;
 
-        while (!quit) {
-            ("Enter row count or quit(q): ");
-            temp_user_input = Console.ReadLine();
-            if (int.TryParse(temp_user_input, out user_input))
+        while (quit == false)
+        {
+            Console.Write("Enter row count or quit(q): ");
+            temp_number_of_rows = Console.ReadLine();
+            if (int.TryParse(temp_number_of_rows, out number_of_rows))
             {
-                
+                miles = 15;
+                Console.WriteLine("MPH\tKPH");
+                for (int i = 1; i <= number_of_rows; i++)
+                {
+                    kms = miles / conversion_rates;
+                    Console.WriteLine("{0}\t{1:f2}", miles, kms);
+
+                    miles = miles + 10;
+                }
             }
-            if (getchar() = 'q')
+            if (char.TryParse(temp_number_of_rows, out quit_char))
             {
-                quit = true;    
+                quit = true;
             }
         }
     }
